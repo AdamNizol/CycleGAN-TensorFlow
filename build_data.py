db2eb3e2c7711f3647227f947dd6518e07be33ff
AdamNizol <32315521+AdamNizol@users.noreply.github.com>
2020-12-19 17:21:49 -0800

    Revert "fix tensorflow import on build_data"
    This reverts commit 7ebf374dd521891cbaa5df420267611aa12bc16b.

--- build_data.py
+++ build_data.py
@@ -1,4 +1,4 @@
-import tensorflow.compat.v1 as tf
+import tensorflow as tf
 import random
 import os
 
@@ -7,7 +7,7 @@ try:
 except ImportError:
   # Python 2 polyfill module
   from scandir import scandir
-
+    
 
 FLAGS = tf.flags.FLAGS
 
